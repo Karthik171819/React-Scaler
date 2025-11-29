9f9007c9c0da00d15b4788c532f47bb825a0a0f5
Karthik171819 <karthikkumar092004@gmail.com>
2025-11-29 14:32:46 +0530

    Main App Componenet

--- test/vite-project/src/App.jsx
+++ test/vite-project/src/App.jsx
@@ -1,5 +1,10 @@
 import Hello from "./components/Hello";
+import Bye from "./components/Bye";
 
 export default function App(){
-  return <Hello/>
+  return <>
+    <Hello/>
+    <Bye/>
+  </>
+  
 }
