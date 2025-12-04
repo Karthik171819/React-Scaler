04c1ac1ad201351df88c6ffb838b5db336d8bd3f
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-04 20:43:35 +0530

    Rendering Array Elements

--- test/vite-project/src/components/RenderingArray.jsx
+++ test/vite-project/src/components/RenderingArray.jsx
@@ -1,12 +1,15 @@
+import Fruit from "./RenderingArrayComponent";
 export default function Fruits(){
-    let fruits = [{nmae:"Apple", price:"100"},
-                  {nmae:"Orange", price:"80"},
-                  {nmae:"Mango", price:"70"},
+    let fruits = [{name:"Apple", price:"100"},
+                  {name:"Orange", price:"80"},
+                  {name:"Mango", price:"70"},
                 ];
     return <div>
         <ol>
             {fruits.map((fruit) => (
-             <h2><li key={fruit.nmae}> {fruit.nmae} -{fruit.price}</li>  </h2> 
+            //  <h2><li key={fruit.name}> {fruit.name} -{fruit.price}</li>  </h2>
+            //component rendering
+            <Fruit name={fruit.name} price={fruit.price}/>
             ))}
         </ol>
     </div>
---
+++ test/vite-project/src/components/RenderingArrayComponent.jsx
@@ -0,0 +1,5 @@
+export default function Fruit({name, price}){
+    return<>
+    <h2><li>The price of {name} is {price}</li></h2>
+    </>
+}
