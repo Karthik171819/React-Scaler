665683d6f6f893bf268fa98a8d2688e615eec2ee
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-05 19:34:12 +0530

    Form handling multiple input event

--- test/vite-project/src/StateEvents and Forms/MultipleInputEvent.jsx
+++ test/vite-project/src/StateEvents and Forms/MultipleInputEvent.jsx
@@ -2,8 +2,15 @@ import { useState } from "react"
 
 export default function Forms(){
     const [name, setName] = useState({firstName:'', lastName:''});
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log({name})
+    }
     return<>
+
     <h2>{name.firstName}-{name.lastName}</h2>
+
     <form>
         <label>  
             <input onChange={(e) => setName({...name, firstName: e.target.value})} type="text" value={name.firstName} placeholder="enter firstname"/>
@@ -12,6 +19,8 @@ export default function Forms(){
         <label>
             <input onChange={(e) => setName({...name, lastName: e.target.value})} type="text" value={name.lastName} placeholder="enter lastname"/>
         </label>
+
+        <button type="submit" onClick={(e) => handleSubmit(e)}>Submit</button>
     </form>
     
        
