56a0c9ec7d76119f2cffb089ea3c1b3019f649f1
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-10 22:11:33 +0530

    movie store

--- movie-store/src/App.jsx
+++ movie-store/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css"
 export default function App(){
   return(
     <div>
-      <h1>Hello React we build movies list</h1>
+      <h1 class="text-4xl font-bold text-blue-600">Hello Tailwind + Vite!</h1>
     </div>
   )
 }
