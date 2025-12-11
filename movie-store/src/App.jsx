ff6a6d5296ce784588708233da2c59cbb4fe4159
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-11 20:58:33 +0530

    movie store

--- movie-store/src/App.jsx
+++ movie-store/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css"
 export default function App(){
   return(
     <div>
-      <h1 class="text-4xl font-bold text-blue-600">Hello Tailwind + Vite!</h1>
+      <h1 class="text-4xl font-bold text-blue-800">Hello Tailwind + Vite!</h1>
     </div>
   )
 }
