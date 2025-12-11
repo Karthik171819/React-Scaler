9b08b12d0ee0b568a82599404b6b3142a503269c
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-11 22:03:30 +0530

    Navbar

--- movie-store/src/components/Navbar.jsx
+++ movie-store/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Logo from "../MovieLogo.png"
 
 const Navbar = () => {
   return (
-    <div className="flex border space-x-8 items-center pi-3 py-4">
+    <div className="flex border space-x-8 items-center pl-3 py-4">
         <img className='w-[50px]' src={Logo} alt=""/>
 
         <a  className="text-blue-500 text-3xl font-bold" href="">Home</a>
