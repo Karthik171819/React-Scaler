4f9ff74f5fb9d26e1a341cf86ee81e0f4d3dff7c
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-11 21:13:14 +0530

    movie store

--- movie-store/src/App.jsx
+++ movie-store/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css"
+import Navbar from "./components/Navbar"
+
 export default function App(){
   return(
     <div>
-      <h1 class="text-4xl font-bold text-blue-800">Hello Tailwind + Vite!</h1>
+      <Navbar/>
     </div>
   )
 }
