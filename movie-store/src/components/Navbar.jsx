5d19e2ba4f787b2cfb2fb90dd79ce2a05d4d2f19
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-11 21:22:04 +0530

    Navbar

--- movie-store/src/components/Navbar.jsx
+++ movie-store/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+//import movie logo
+import Logo from "../MovieLogo.png"
+
 const Navbar = () => {
   return (
     <div>
-        <img src={} alt=""/>
+        <img src={Logo} alt=""/>
     </div>
   )
 }
