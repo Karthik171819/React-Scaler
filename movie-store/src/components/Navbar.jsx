54e4b87fa1510473bb0bc00f3c403b78b85a22b5
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-11 22:02:49 +0530

    movie store

--- movie-store/src/components/Navbar.jsx
+++ movie-store/src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = () => {
     <div className="flex border space-x-8 items-center pi-3 py-4">
         <img className='w-[50px]' src={Logo} alt=""/>
 
-        <a  className="text-blue-500" href="">Home</a>
+        <a  className="text-blue-500 text-3xl font-bold" href="">Home</a>
 
-        <a className="text-blue-500" href="/watchlist">WatchList</a>
+        <a className="text-blue-500 text-3xl font-bold " href="/watchlist">WatchList</a>
     </div>
   )
 }
