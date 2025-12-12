728a5c6e0d0f27afb239273537b80349d639583c
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-12 20:18:51 +0530

    WatchList

--- movie-store/src/App.jsx
+++ movie-store/src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css"
 import Navbar from "./components/Navbar"
+import Movies from "./components/Movies"
+import WatchList from "./components/WatchList"
 
 export default function App(){
   return(
     <div>
       <Navbar/>
+
+      <Movies/>
+
+      <WatchList/>
     </div>
   )
 }
