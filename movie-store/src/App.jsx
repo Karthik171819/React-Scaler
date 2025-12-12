9da9882c99f49ce900797dfaceef05c375d3b1ad
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-12 20:30:00 +0530

    App

--- movie-store/src/App.jsx
+++ movie-store/src/App.jsx
@@ -3,14 +3,27 @@ import Navbar from "./components/Navbar"
 import Movies from "./components/Movies"
 import WatchList from "./components/WatchList"
 
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 export default function App(){
   return(
     <div>
-      <Navbar/>
+      <BrowserRouter>
 
-      <Movies/>
 
-      <WatchList/>
+        <Navbar/>
+
+          <Routes>
+
+
+            <Movies/>
+
+            <WatchList/>
+          </Routes>
+        
+      </BrowserRouter>
+
+      
     </div>
   )
 }
