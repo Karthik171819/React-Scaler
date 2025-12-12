ea7db30f43c8bc1bc469fd5ceb7a7ff20020d59e
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-12 20:45:47 +0530

    App Routing

--- movie-store/src/components/Navbar.jsx
+++ movie-store/src/components/Navbar.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 //import movie logo
 import Logo from "../MovieLogo.png"
 
+import {Link} from 'react-router-dom'
+
 const Navbar = () => {
   return (
     <div className="flex border space-x-8 items-center pl-3 py-4">
         <img className='w-[50px]' src={Logo} alt=""/>
 
-        <a  className="text-blue-500 text-3xl font-bold" href="">Home</a>
+        <Link to="/"  className="text-blue-500 text-3xl font-bold" >Movies</Link>
 
-        <a className="text-blue-500 text-3xl font-bold " href="/watchlist">WatchList</a>
+        <Link to="/watchlist" className="text-blue-500 text-3xl font-bold " >WatchList</Link>
     </div>
   )
 }
