4a886c686c5df6656ad2019480bc6626ecd733a6
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-15 23:17:50 +0530

    App Routing

--- movie-store/src/components/banner.jsx
+++ movie-store/src/components/banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const banner = () => {
   return (
-    <div className='md:h-[110vh] bg-cover bg-center' style={{backgroundImage: 'url(https://images.ctfassets.net/3sjsytt3tkv5/48dw0Wqg1t7RMqLrtodjqL/d72b35dae2516fa64803f4de2ab8e30f/Avengers-_Endgame_-_Header_Image.jpeg)'}}> 
+    <div className=' h-[20vh] md:h-[80vh] bg-cover bg-center' style={{backgroundImage: 'url(https://images.ctfassets.net/3sjsytt3tkv5/48dw0Wqg1t7RMqLrtodjqL/d72b35dae2516fa64803f4de2ab8e30f/Avengers-_Endgame_-_Header_Image.jpeg)'}}> 
 
     </div>
   )
