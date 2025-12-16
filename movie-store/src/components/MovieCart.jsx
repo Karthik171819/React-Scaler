449dc6aa2658a13052c13d4edf0f572b5bba22d9
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-16 20:23:59 +0530

    Movies Cart

---
+++ movie-store/src/components/MovieCart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieCart = () => {
+  return (
+    <div>Movie Card</div>
+  )
+}
+
+export default MovieCart
--- movie-store/src/components/Movies.jsx
+++ movie-store/src/components/Movies.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import MovieCart from './MovieCart'
 
 const Movies = () => {
   return (
     <div>
-        <div>Trending Movies</div>
+        <div className='text-2xl text-center font-bold m-5'>
+          Trending Movies
+        </div>
+
+        <div>
+          <MovieCart/>
+        </div>
     </div>
   )
 }
