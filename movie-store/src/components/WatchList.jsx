4b9610c4641e1c73346edbb231c5c5ad5222c277
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-22 15:04:35 +0530

    watch list

--- movie-store/src/components/WatchList.jsx
+++ movie-store/src/components/WatchList.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 const WatchList = () => {
   return (
-    <div>
-        <h1>Watch List</h1>
-    </div>
+    <>
+      <div className='flex justify-center'>
+        <input type="text" placeholder="Search Movie" className='border-2 border-gray-400 bg-gray-200 p-2 m-4 rounded-lg w-1/3 '/>
+      </div>
+
+      <div>
+        <table>
+          <thead>
+            
+
+
+          </thead>
+
+          <tbody>
+
+
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
