a8c15c2a720cadf0891c9c9a87a285f2c33ab7e7
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-22 11:54:11 +0530

    use effect fetching api dynamically

--- movie-store/src/components/MovieCard.jsx
+++ movie-store/src/components/MovieCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const MovieCard = ({poster_path}) => {
+const MovieCard = ({poster_path, name}) => {
   return (
     <div  className='h-[40vh] w-[200px] bg-center bg-cover rounded-xl px-3 hover:scale-110 duration-300 hover:cursor-pointer ' 
     style={{
       backgroundImage : `url(https://image.tmdb.org/t/p/original/${poster_path})`
       }}>
+        <div>
+          {name}
+        </div>
     </div>
   )
 }
