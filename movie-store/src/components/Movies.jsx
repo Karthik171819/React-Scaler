b1a41a4513ecdb4754b36f03760121d187249617
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-22 11:30:53 +0530

    use effect fetching api

--- movie-store/src/components/Movies.jsx
+++ movie-store/src/components/Movies.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MovieCard from './MovieCard'
+import axios from 'axios'
 
 const Movies = () => {
+
+  useEffect(() =>{
+    axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=6849083db304fcea8fc082607b7f0d4f&language=en-US&page=2%27`)
+    .then(function(res){
+      console.log(res)
+    })
+  }, [])
+  
   return (
     <div>
         <div className='text-2xl text-center font-bold m-5'>
@@ -10,14 +19,12 @@ const Movies = () => {
 
         <div className='flex flex-row flex-wrap justify-around'>
           <MovieCard/>
-          <MovieCard/>
-          <MovieCard/>
-          <MovieCard/>
-          <MovieCard/>
-          <MovieCard/>
+          
         </div>
     </div>
   )
 }
 
 export default Movies
+
+//https://api.themoviedb.org/3/movie/popular?api_key=6849083db304fcea8fc082607b7f0d4f&language=en-US&page=2%27
