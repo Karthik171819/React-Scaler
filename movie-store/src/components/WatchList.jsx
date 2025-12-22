a686dc92c811d749e2386a9d47d39b9cbac81d12
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-22 15:22:54 +0530

    watch list

--- movie-store/src/components/WatchList.jsx
+++ movie-store/src/components/WatchList.jsx
@@ -7,11 +7,16 @@ const WatchList = () => {
         <input type="text" placeholder="Search Movie" className='border-2 border-gray-400 bg-gray-200 p-2 m-4 rounded-lg w-1/3 '/>
       </div>
 
-      <div>
-        <table>
-          <thead>
-            
+      <div className='border border-gray-200 m-8'>
+        <table className='text-gray-500 text-center w-full'>
 
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Ratings</th>
+              <th>Popularity</th>
+              <th>Genre</th>
+            </tr>
 
           </thead>
 
