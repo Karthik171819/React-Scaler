be06cf7d6c20d7f524d4fa8c118d5eba6d2ff679
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-22 16:06:01 +0530

    watch list

--- movie-store/src/components/WatchList.jsx
+++ movie-store/src/components/WatchList.jsx
@@ -3,11 +3,18 @@ import React from 'react'
 const WatchList = () => {
   return (
     <>
+
+     <div className='flex justify-center flex-wrap m-4'>
+        <div className=' flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-blue-400 mx-5'>Action</div>
+        <div className=' flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-gray-400 mx-5'>All genre</div>
+        <div className=' flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-gray-400 mx-5'>Action</div>
+     </div>
+
       <div className='flex justify-center'>
         <input type="text" placeholder="Search Movie" className='border-2 border-gray-400 bg-gray-200 p-2 m-4 rounded-lg w-1/3 '/>
       </div>
 
-      <div className='border border-gray-200 m-8'>
+      <div className=' overflow-hidden rounded-lg border border-gray-200 m-8'>
         <table className='text-gray-500 text-center w-full'>
 
           <thead className='border-b-2'>
@@ -32,7 +39,7 @@ const WatchList = () => {
               <td>8.5</td>
               <td>9</td>
               <td>Action</td>
-              <td className='text-red-600'>Delete</td>
+              <td className='text-red-600 hover:cursor-pointer'>Delete</td>
             </tr>
 
 
