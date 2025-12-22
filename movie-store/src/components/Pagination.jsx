13a09454ea2e50f47863c2557947a687bfbeb1b3
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-22 12:42:19 +0530

    Pagination

--- movie-store/src/components/Pagination.jsx
+++ movie-store/src/components/Pagination.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Pagination = () => {
   return (
-    <div className='bg-gray-400 p-4 mt-8 flex justify-center'>Pagination</div>
+    <div className='bg-gray-400 p-4 mt-8 flex justify-center'>
+        <div className='px-5 hover:cursor-pointer'><i class="fa-solid fa-arrow-left"></i></div>
+        <div>1</div>
+        <div className='px-5 hover:cursor-pointer'><i class="fa-solid fa-arrow-right"></i></div>
+    </div>
   )
 }
 
