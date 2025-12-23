2cd083d0e75911fe94802695893983f0152e2d4f
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-23 20:13:33 +0530

    App.jsx

--- movie-store/src/App.jsx
+++ movie-store/src/App.jsx
@@ -5,8 +5,17 @@ import WatchList from "./components/WatchList"
 import Banner from "./components/Banner"
 
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { useState } from "react"
 
 export default function App(){
+
+  let [watchList, setWatchList] = useState([])
+
+  let handleWatchList = (movieObj) => {
+    let newWatchList = [...watchList, movieObj]
+    setWatchList(newWatchList)  
+    console.log(watchList);
+  }
   return (
     <div>
       <BrowserRouter>
