07f4be24a03a25caa50d7df60c89b7ebe258a8a0
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-24 16:02:02 +0530

    Watch List

--- movie-store/src/components/WatchList.jsx
+++ movie-store/src/components/WatchList.jsx
@@ -1,66 +1,89 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const WatchList = ({watchList}) => {
+const WatchList = ({ watchList }) => {
+  const [search, setSearch] = useState("");
 
-  const [search, setSearch] = useState("")
-
-  let handleSearch=(e)=>{
-    setSearch(e.target.value)
-  }
+  let handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <>
-
-     <div className='flex justify-center flex-wrap m-4'>
-        <div className=' flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-blue-400 mx-5'>All Genres</div>
-        <div className=' flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-gray-400 mx-5'>Action</div>
-        <div className=' flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-gray-400 mx-5'>Crime</div>
-        <div className=' flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-gray-400 mx-5'>Comedy</div>
-     </div>
-
-      <div className='flex justify-center'>
-        <input onChange={handleSearch} value={search} type="text" placeholder="Search Movie" className='border-2 border-gray-400 bg-gray-200 p-2 m-4 rounded-lg w-1/3 '/>
+      <div className="flex justify-center flex-wrap m-4">
+        <div className=" flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-blue-400 mx-5">
+          All Genres
+        </div>
+        <div className=" flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-gray-400 mx-5">
+          Action
+        </div>
+        <div className=" flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-gray-400 mx-5">
+          Crime
+        </div>
+        <div className=" flex justify-center h-[3rem] w-[9rem] rounded-xl text-white font-bold items-center bg-gray-400 mx-5">
+          Comedy
+        </div>
       </div>
 
-      <div className=' overflow-hidden rounded-lg border border-gray-200 m-8'>
-        <table className='text-gray-500 text-center w-full'>
+      <div className="flex justify-center">
+        <input
+          onChange={handleSearch}
+          value={search}
+          type="text"
+          placeholder="Search Movie"
+          className="border-2 border-gray-400 bg-gray-200 p-2 m-4 rounded-lg w-1/3 "
+        />
+      </div>
 
-          <thead className='border-b-2'>
-
+      <div className=" overflow-hidden rounded-lg border border-gray-200 m-8">
+        <table className="text-gray-500 text-center w-full">
+          <thead className="border-b-2">
             <tr>
               <th>Name</th>
-              <th>Ratings</th>
+
+              <div>
+                <div></div>
+                <th>Rating</th>
+                <div></div>
+              </div>
+
               <th>Popularity</th>
               <th>Genre</th>
             </tr>
-
           </thead>
 
           <tbody>
+            {watchList
+              .filter((movieObj) => {
+                return movieObj.title
+                  .toLowerCase()
+                  .includes(search.toLowerCase());
+              })
+              .map((movieObj) => {
+                return (
+                  <tr className="border-b">
+                    <td className="flex items-center px-6 py-4">
+                      <img
+                        className="w-20 h-20"
+                        src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`}
+                      />
+                      <span className="ml-6 font-semibold">
+                        {movieObj.title}
+                      </span>
+                    </td>
 
-            {watchList.filter((movieObj) => {
-              return movieObj.title.toLowerCase().includes(search.toLowerCase())  
-            }).map((movieObj) => {
-              return  <tr className='border-b'>
-              <td className='flex items-center px-6 py-4'>
-                <img className="w-20 h-20" src={`https://image.tmdb.org/t/p/original/${movieObj.poster_path}`} />
-                <span className='ml-6 font-semibold'>{movieObj.title}</span>
-              </td>
-
-              <td>{movieObj.vote_average}</td>
-              <td>{movieObj.popularity}</td>
-              <td>{movieObj.genre_ids}</td>
-              <td className='text-red-600 hover:cursor-pointer'>Delete</td>
-            </tr>
-
-            })}
-
-
-
+                    <td>{movieObj.vote_average}</td>
+                    <td>{movieObj.popularity}</td>
+                    <td>{movieObj.genre_ids}</td>
+                    <td className="text-red-600 hover:cursor-pointer">
+                      Delete
+                    </td>
+                  </tr>
+                );
+              })}
           </tbody>
         </table>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default WatchList
+export default WatchList;
