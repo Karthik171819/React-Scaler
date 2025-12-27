ff7fbf5d499c00ffc4d568f231a3af11a54a855f
Karthik171819 <karthikkumar092004@gmail.com>
2025-12-27 12:49:40 +0530

    context api

--- context-API/vite-project/src/prop-drill/GrandSon.jsx
+++ context-API/vite-project/src/prop-drill/GrandSon.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FamilyContext } from './FamilyContext'
+import { useContext } from 'react';
 
 function GrandSon() {
+  const familyMessage = useContext(FamilyContext);
   return (
     <div className='gson'>
-        <h3>GrandSon </h3>
+        <h3>GrandSon {familyMessage.familyName}</h3>
     </div>
   )
 }
--- context-API/vite-project/src/prop-drill/Parent.jsx
+++ context-API/vite-project/src/prop-drill/Parent.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Child from './Child'
+import { FamilyContext } from './FamilyContext'
+import { useContext } from 'react';
 
 function Parent() {
+  const familyMessage = useContext(FamilyContext);
   return (
     <div className="parent">
-        <h1>Parent</h1>
+        <h1>Parent {familyMessage.familyName}</h1>
         <Child />
     </div>
   )
